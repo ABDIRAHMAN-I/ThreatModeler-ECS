af59a2dfb24efea7692e8c06f0ec7d6dace37b4b
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2024-11-23 00:42:34 +0000

    created terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-threatmodel-statefile-bucket"
-    key = "state-file"
-    region = "eu-west-2"
+    bucket  = "my-threatmodel-statefile-bucket"
+    key     = "state-file"
+    region  = "eu-west-2"
     encrypt = true
   }
 }

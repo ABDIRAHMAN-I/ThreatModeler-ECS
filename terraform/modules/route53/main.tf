ee1709a393eea2d2fc1b215cb9853c3e64af3e58
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2024-11-26 19:26:29 +0000

    made changes to my route53 module

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "tm_lab_zone" {
 resource "aws_route53_record" "tm_cname_record" {
   zone_id = aws_route53_zone.tm_lab_zone.id
   name    = var.record_name
-  type    = "CNAME"
-  ttl     = 300
+  type    = var.record_type
+  ttl     = var.ttl
   records = [var.alb_dns_name]
 }
--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -1,22 +1,22 @@
 variable "zone_name" {
-  description = "Route53 zone name"
   type        = string
-  default     = "threatmodelprojectabdirahmanismail.uk" 
 }
 
 variable "record_name" {
-  description = "Route53 record name"
   type        = string
-  default     = "lab.threatmodelprojectabdirahmanismail.uk" 
+}
+
+variable "record_type" {
+  type = string
+  default = "CNAME"
+  
 }
 
 variable "alb_dns_name" {
-  description = "The DNS name of the ALB to point the Route53 record to"
   type        = string
 }
 
 variable "lb_zone_id" {
-  description = "The zone ID of the load balancer"
   type        = string
 }
 

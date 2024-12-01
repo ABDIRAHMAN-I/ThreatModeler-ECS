2f6be00b450a526a05355faa1fc59200704edb65
ABDIRAHMAN-I <abdirahman.i@hotmail.com>
2024-12-01 02:26:13 +0000

    added created dynamodb to backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket  = "my-threatmodel-statefile-bucket"
     key     = "state-file"
     region  = "eu-west-2"
+    dynamodb_table = "dydb-state-locking-ecs"
     encrypt = true
+
   }
 }
